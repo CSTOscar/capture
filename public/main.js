7f458f5fe825d2ae7ce72271ebf157704f024f96
Charles J.Y. Yoon <1242029+jaeyeun97@users.noreply.github.com>
2018-03-04 00:56:34 +0000

    info sync done, need to implement capture

--- public/main.js
+++ public/main.js
@@ -1,17 +1,9 @@
-'use strict';
-/*
-var socket = io.connect('http://' + document.domain + ':' + location.port);
-
-socket.on('connect', function() {
-	socket.emit('my event', {data: 'I\'m connected!'});
-});
-*/
-
+var socket = io.connect('https://' + document.domain+'/', {path: "/capture/socket.io"});
 var video = document.querySelector('video#live');
 var select = document.querySelector('select#videoSource');
 
-function gotDevices(deviceInfos) {
-	var values = select.value;
+var gotDevices = function(deviceInfos) {
+	var value = select.value;
 
 	while (select.firstChild) {
 		select.removeChild(select.firstChild);
@@ -24,27 +16,25 @@ function gotDevices(deviceInfos) {
 		if (deviceInfo.kind === 'videoinput') {
 			option.text = deviceInfo.label || 'camera ' + (select.length + 1);
 			select.appendChild(option);
-		} else {
-			console.log('Some other kind of source/device: ', deviceInfo);
 		}
 	}
 	if (Array.prototype.slice.call(select.childNodes).some(function(n) {
-		return n.value === values;
+		return n.value === value;
 	})) {
-		select.value = values;
+		select.value = value;
 	}
 }
 
 navigator.mediaDevices.enumerateDevices().then(gotDevices).catch(handleError);
 
-function gotStream(stream) {
+var gotStream = function(stream) {
 	window.stream = stream; // make stream available to console
 	video.srcObject = stream;
 	// Refresh button list in case labels have become available
 	return navigator.mediaDevices.enumerateDevices();
 }
 
-function start() {
+var start = function() {
 	if (window.stream) {
 		window.stream.getTracks().forEach(function(track) {
 			track.stop();
@@ -58,10 +48,52 @@ function start() {
 		.then(gotStream).then(gotDevices).catch(handleError);
 }
 
+var handleError = function (error) {
+	console.log('navigator.getUserMedia error: ', error);
+}
+
 select.onchange = start;
 
 start();
 
-function handleError(error) {
-	console.log('navigator.getUserMedia error: ', error);
+
+$('#connect').on('click', function(){
+	socket.emit('start');
+})
+
+$('#disconnect').on('click', function(){
+	socket.emit('remove', {'camera_id': window.camera_id, 'side': window.side});
+})
+
+$('#clear').on('click', function(){
+	socket.emit('clear');
+})
+
+socket.on('connected', function(data){
+	console.log('connected as ' + data.camera_id + 'with side '+ data.side);
+	window.camera_id = data.camera_id;
+	window.side = data.side;
+	$('#side').val(data.side);
+	socket.emit('ready');
+});
+
+var capture = function(){
+	console.log('Record here');
 }
+
+socket.on('startRecording', function(data){
+	var time = new Date(data.time);
+	console.log('Starting Recording at: ' + time);
+	setTimeout(function() {
+		capture();
+		setTimeout(capture, data.interval);
+	}, time - new Date());
+});
+
+socket.on('wait', function(){
+	console.log("Waiting...");
+});
+
+socket.on('error', function(data){
+	console.log(data.error);
+});
--- server/app.py
+++ server/app.py
@@ -1,7 +1,65 @@
-from flask import Flask, render_template
-from flask_socketio import SocketIO
+from flask import Flask, render_template, session
+from flask_socketio import SocketIO, send, emit
+from datetime import datetime, timedelta
+from uuid import uuid4
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
-socketio = SocketIO(app)
+sio = SocketIO(app)
 
+cameras = dict()
+
+
+@sio.on('start')
+def start():
+    if len(cameras) >= 2:
+        emit('error', {'error': 'Too many cameras'})
+    else:
+        session['camera_id'] = str(uuid4());
+        payload = {'camera_id': session['camera_id']}
+        if 'left' not in cameras.values():
+            cameras[session['camera_id']] = 'left';
+            payload.update({'side': 'left'})
+        else:
+            cameras[session['camera_id']] = 'right';
+            payload.update({'side': 'right'})
+        emit('connected', payload, json=True)
+
+
+@sio.on('ready')
+def ready():
+    print(cameras)
+    if len(cameras) == 2:
+        emit('startRecording', {
+            'time': (datetime.utcnow() + timedelta(seconds=2)).isoformat(),
+            'interval':  200 # in ms
+        }, broadcast=True)
+    else:
+        emit('wait')
+
+
+@sio.on('remove')
+def remove(data):
+    cid = data.get('camera_id')
+    if cid in cameras.keys():
+        del cameras[cid]
+    print(cid + "disconnected.")
+    print(data)
+
+
+@sio.on('capture')
+def capture(data):
+    # decrypt image & time out of data
+    # store image somewhere for test 
+    # add it to list of images
+    print("Start Capturing")
+    pass
+
+
+@sio.on('clear')
+def clear():
+    for k in cameras.keys():
+        del cameras[k]
+
+if __name__ == "__main__":
+    sio.run(app, '0.0.0.0', 8000, use_reloader=True, log_output=True) 
